3600fe7b6f69d6587ebb87e1d8b8030826db8222
Eugène Fanera <136158200+rahFanera@users.noreply.github.com>
2025-08-10 07:19:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { Toaster } from "react-hot-toast";
 import BackgroundBlobs from "./components/BackgroundBlobs";
 import MainCard from "./components/MainCard";
 
 export default function App() {
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen text-center overflow-hidden bg-gray-950 px-4 py-8">
-      <Toaster />
       <BackgroundBlobs />
       <div className="absolute inset-0 bg-[radial-gradient(white,transparent_1px)] bg-[length:3px_3px] opacity-10"></div>
       <MainCard />
